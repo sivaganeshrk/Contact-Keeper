e3213500233db9fe720393d4e660a96a40d36ce5
Siva Ganesh <sivadoog0808@gmail.com>
2020-05-21 22:47:07 +0530

    Update the dtatbase connectivity and the password hashing

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
 const { check, validationResult } = require("express-validator");
 
@@ -16,13 +17,37 @@ router.post(
       "Please enter a password with 6 or more character"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("Passed...");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exist" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      return res.send("User added");
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
 );
 
