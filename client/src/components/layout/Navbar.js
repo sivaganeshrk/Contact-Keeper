ec69498d9fa7a294565d041c10ea526c230beb63
Siva Ganesh <sivadoog0808@gmail.com>
2020-05-25 21:27:59 +0530

    Do some basic cleanup and created the app.css and created nav bar

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  name: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Contact Keeper",
+  icon: "fas fa-id-card-alt",
+};
+
+export default Navbar;
