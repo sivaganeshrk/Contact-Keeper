98f654799f359dfb8e6a313fc3140680c2e58566
Siva Ganesh <sivadoog0808@gmail.com>
2020-05-31 21:46:51 +0530

    Added the basic validation and alert and its components(UI)

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import AlertContext from "../../context/alert/alertContext";
 
 const Register = () => {
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -12,45 +17,60 @@ const Register = () => {
   const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("Register Submited");
+    if (name === "" || email === "" || password === "") {
+      setAlert("Please enter all fields", "danger");
+    } else if (password !== password2) {
+      setAlert("Password do not match", "danger");
+    } else {
+      console.log("Register Submit");
+    }
   };
   return (
     <div className="form-container">
       <h1>
         Account <span className="text-primary">Register</span>
-        <form onSubmit={onSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" value={name} onChange={onChange} />
-            <label htmlFor="email">Email Address</label>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={onChange}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={onChange}
-            />
-            <label htmlFor="password2">Confirm Password</label>
-            <input
-              type="password"
-              name="password2"
-              value={password2}
-              onChange={onChange}
-            />
-          </div>
-          <input
-            type="submit"
-            value="Register"
-            className="btn btn-primary btn-block"
-          />
-        </form>
       </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Enter your name..."
+          />
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            placeholder="Enter your email-id "
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            placeholder="Enter your password"
+          />
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            placeholder="Re-enter your password"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        />
+      </form>
     </div>
   );
 };
