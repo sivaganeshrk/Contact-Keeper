e6461e010baff283a2d3b3505dde8a7d814bc672
Siva Ganesh <sivadoog0808@gmail.com>
2020-06-04 10:56:11 +0530

    Prepare for deploy

---
+++ client/build/precache-manifest.bae7b4c667748e564b879f60fb29adb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1123e251a04d35d00bc975a0b557fb6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a789a311d91eafcdec",
+    "url": "/static/css/main.00085ea3.chunk.css"
+  },
+  {
+    "revision": "92e5db2999734d4ce97d",
+    "url": "/static/js/2.1b6a163c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b6a163c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a789a311d91eafcdec",
+    "url": "/static/js/main.305cc4f7.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/db");
-
+const path = require("path");
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-  res.json({ msg: "Wlcome to Contact Keeper API...." })
-);
-
-//Definng Routes
+//Defining Routes
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
